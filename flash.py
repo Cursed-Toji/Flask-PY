0b1632b8d6271d35e6f0b90e9e9ada381229a9d4
Cals <88802566+victorcals@users.noreply.github.com>
2024-05-25 21:10:35 -0300

    Update flash.py

--- flash.py
+++ flash.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -15,6 +15,16 @@ class Post(db.Model):
     title = db.Column(db.String(80), unique=True, nullable=False)
     content = db.Column(db.Text, nullable=False)
     author = db.Column(db.String(80), nullable=False)
+
+    def to_dict(self):
+        result = {}
+        for key in self.mapper.c.keys():
+            in getattr(self, key) is not None:
+            result[key] = str(getattr(self, key))
+        else:
+            result[key] = getattr(self, key)
+
+        return  result
 
 @app.route('/')
 def home():
@@ -60,6 +70,59 @@ def edit_post(id):
         return redirect(url_for('home'))
     else:
         return render_template('edit.html', post=post)
+
+
+
+
+
+########################################################
+
+@app.route('/api/posts')
+def api_lists_posts():
+    posts = Post.query.all()
+    return [post.to_dict() for post in posts]
+
+# @app.route('/api/post/add', methods=['POST'])
+# def api_add_post():
+#     try:
+#         form = request.form
+#         post = Post(title=form['title'], content=form['content'], author=form['author'])
+#         db.session.add(post)
+#         db.session.commit()
+#     except Exception as e:
+#         return str(e)
+#
+#     return redirect(url_for('home'))
+#
+# @app.route('/api/post/<int:id>/del', methods=['POST'])
+# def api_delete_post(id):
+#     try:
+#         post = Post.query.get(id)
+#         if post:
+#             db.session.delete(post)
+#             db.session.commit()
+#     except Exception as e:
+#         return str(e)
+#
+#     return redirect(url_for('home'))
+#
+# @app.route('/post/<int:id>/edit', methods=['POST', 'GET'])
+# def edit_post(id):
+#     post = Post.query.get(id)
+#     if request.method == 'POST':
+#         try:
+#             form = request.form
+#             post.title = form['title']
+#             post.content = form['content']
+#             post.author = form['author']
+#             db.session.commit()
+#         except Exception as e:
+#             return str(e)
+#         return redirect(url_for('home'))
+#     else:
+#         return render_template('edit.html', post=post)
+#
+
 
 # Inicialização do banco de dados
 with app.app_context():
